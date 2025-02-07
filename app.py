8589b0b862c8aa13d5111dda5f23d8e88e3bb4f6
JoannaZhang-cs <jzsunset10@gmail.com>
2025-02-07 10:16:29 -0500

    2/7 booleans work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-x = 3
+""" x = 3
 y = float(3)
 print(x,y)
 
@@ -8,13 +8,105 @@ for i in values:
     print(i)
 
 print(values[0])
-print(values[6])
+print(values[6]) """
 
 # Print the element 7
-print(values[3])  # This prints 7
+""" print(values[3])  # This prints 7
 
 x = "this is a thing"
-y= x.split( )
+y= x.split( ) #split() separates the string at any whitespace (spaces, newlines, etc.)
 z = y[0]
 print(y)
-print(z)
+print(z) """
+
+#booleans
+""" def login(password):
+    #if statement evaluates to true we go to next line
+    if password == "secret":
+        print("logged in")
+    else:
+        print("incorrect")
+x = input("what is the passoword")
+login(x) """
+
+""" def grade(score):
+    if score >=92:
+        print("A")
+    elif score>= 82:
+        print("B")
+    elif score >= 72:
+        print("C")
+    else:
+        print("F")
+#add integer (int) if you need the answer to be an integer
+x = int(input("what is the score"))
+grade(x) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" 
+x = "test"
+print(f"hello {x}")
+
+temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+""" def gamble(age, id):
+    if age >= 21:
+        if id:
+        #if id == True means the same thing
+            print("Gamble away")
+        else: 
+            print("You need Id verification")
+    else:
+        print("You're too young")
+ """
+
+""" def gamble(age, id):
+    if age >= 21 and id == True
+    #if id == True means the same thing as if id
+        print("have fun")
+    elif age >= 21 and id == False: 
+        print("You need Id verification")
+    else:
+        print("You're too young")
+ """
+""" raining = False
+if not raining == true:
+    print("go for walk")
+if raining == false:
+    print("go for walk")
+#these are the same """
+
+""" print(12 % 5)
+#% gives you the remainder
+x = "test"
+print(f"hi {x}")
+
+number = 68
+if (% = 0):
+    print('even')
+else: 
+    print('odd')
+ """
+
+bill = good
+def bill(service):
+    if service == bad:
+     print("0%")
+    elif service == okay:
+        print("15%")
+    elif service == good:
+        print("20%")
+    else:
+        print("25%")
+x = input("I'll take the bill")
